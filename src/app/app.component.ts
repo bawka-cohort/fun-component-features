b9a6d6e2afebea1c1a609502eceebf0380841c8f
shomey9 <adyrendusara@gmail.com>
2025-07-25 23:52:04 -0500

    Add navigation for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,16 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
+  template: `
+  <p>Add your name to the url path to get to your component
+  <ul>
+    <li><a href="http://localhost:4200/brian">Brian</a></li>
+    <li><a href="http://localhost:4200/angga">Angga</a></li>
+    <li><a href="http://localhost:4200/ady">Ady</a></li>
+    <li><a href="http://localhost:4200/will">Will</a></li>
+    <li><a href="http://localhost:4200/kevin">Kevin</a></li>
+  </ul>
+  <router-outlet></router-outlet>`,
   styles: ``
 })
 export class AppComponent {
