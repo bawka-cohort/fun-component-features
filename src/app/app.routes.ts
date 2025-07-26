699132978ee9c456cbd4e1ea0ae72ffdf4585c93
shomey9 <adyrendusara@gmail.com>
2025-07-25 23:37:48 -0500

    Add routing for individual contributor base components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { AdyComponent } from './components/ady-components/ady-component';
+import { AnggaComponent } from './components/angga-components/angga-component';
+import { BrianComponent } from './components/brian-components/brian-component';
+import { KevinComponent } from './components/kevin-components/kevin-component';
+import { WillComponent } from './components/will-components/will-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'ady',
+    component: AdyComponent,
+  },
+  {
+    path: 'angga',
+    component: AnggaComponent,
+  },
+  {
+    path: 'brian',
+    component: BrianComponent,
+  },
+  {
+    path: 'kevin',
+    component: KevinComponent,
+  },
+  {
+    path: 'will',
+    component: WillComponent,
+  }
+];
