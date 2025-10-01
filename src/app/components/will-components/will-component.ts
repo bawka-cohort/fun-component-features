f382ed499225ef29b2dd4c27d793046502470224
wwllnn <vvllxn@gmail.com>
2025-10-01 14:13:04 -0500

    Update will-component.ts
    template change

--- src/app/components/will-components/will-component.ts
+++ src/app/components/will-components/will-component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'will-component',
   imports: [],
-  template: `<h1>Say hello to Will hello hi</h1>`,
+  template: `<h1>this is will component</h1>`,
   styles: ``
 })
 export class WillComponent {
